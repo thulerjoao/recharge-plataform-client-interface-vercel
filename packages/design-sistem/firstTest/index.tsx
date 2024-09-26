77df6a07e84d042fe6d105defa2da5f9de75e073
thulerjoao <thuler_lima@hotmail.com>
2024-09-26 20:35:20 +0000

    chore: set up packages dependence and global package.json in the workspace

---
+++ packages/design-sistem/firstTest/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FirstTest = () => {
+  return <h2>Test packages</h2>;
+};
+
+export default FirstTest;
--- projects/web-public/src/app/page.tsx
+++ projects/web-public/src/app/page.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import FirstTest from "@4miga/design-sistem/firstTest/index";
 
 const homeScreen = () => {
   return (
     <main>
       <h1>4miga recharge store</h1>
+      <FirstTest />
     </main>
   );
 };
