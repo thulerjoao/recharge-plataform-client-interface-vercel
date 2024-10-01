02eecc70f91dd030c59a3aa2db80d576f3c52d01
thulerjoao <thuler_lima@hotmail.com>
2024-10-01 17:27:11 +0000

    chore: impelemnte testing react library

--- packages/design-system/components/Text/index.tsx
+++ packages/design-system/components/Text/index.tsx
@@ -3,7 +3,9 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledTxt = styled.span``;
+const StyledTxt = styled.span`
+  font-family: sans-serif;
+`;
 
 interface Props {
   tag?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span" | "u";
