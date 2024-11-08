43d5fd98d12c6134ab6c9ba2c50aa7f9102d610c
thulerjoao <thuler_lima@hotmail.com>
2024-11-08 20:51:03 +0000

    refactor(reseller): componentized saledHeader and Pagination components in sales page

--- projects/reseller/src/app/sales/util/sales.tsx
+++ projects/reseller/src/app/sales/util/sales.tsx
@@ -1,43 +1,18 @@
 import Text from "@4miga/design-system/components/Text";
-import Input from "@4miga/design-system/components/input";
 import { Theme } from "@4miga/design-system/theme/theme";
 import OrderCard from "public/cards/orderCard/card";
 import HeaderEnviroment from "public/components/headerEnviroment";
-import DownArrow from "../common/icons/DownArrow.svg";
-import Search from "../common/icons/Search.svg";
-import Setting from "../common/icons/Setting.svg";
 import Card1 from "../common/temp/Card1.png";
 import Card2 from "../common/temp/Card2.png";
 import { SalesContainer } from "./style";
+import Pagination from "public/components/pagination";
+import SalesHeader from "../common/components/salesHeaderContainer";
 
 const Sales = () => {
   return (
     <SalesContainer>
       <HeaderEnviroment>
-        <header>
-          <div className="headerTitle">
-            <Text fontName="LARGE_SEMI_BOLD">VENDAS</Text>
-          </div>
-          <div className="searchInput">
-            <Input
-              padding="0 44px 0 16px"
-              rightElement={<Search />}
-              placeholder="Pesquisar..."
-              height={40}
-            />
-          </div>
-          <div className="filter">
-            <span>
-              <Setting />
-            </span>
-            <Text align="center" fontName="SMALL">
-              FILTRAR
-            </Text>
-            <span>
-              <DownArrow />
-            </span>
-          </div>
-        </header>
+        <SalesHeader />
       </HeaderEnviroment>
       <section className="titles">
         <span className="orderNumber">
@@ -168,54 +143,7 @@ const Sales = () => {
           rechargeStatus="processing"
         />
       </div>
-      <div className="pagination">
-        <Text align="center" fontName="REGULAR_SEMI_BOLD">
-          1
-        </Text>
-        <Text
-          align="center"
-          color={Theme.colors.secondaryText}
-          fontName="REGULAR_SEMI_BOLD"
-        >
-          2
-        </Text>
-        <Text
-          align="center"
-          color={Theme.colors.secondaryText}
-          fontName="REGULAR_SEMI_BOLD"
-        >
-          3
-        </Text>
-        <Text
-          align="center"
-          color={Theme.colors.secondaryText}
-          fontName="REGULAR_SEMI_BOLD"
-        >
-          4
-        </Text>
-        <Text
-          align="center"
-          color={Theme.colors.secondaryText}
-          fontName="REGULAR_SEMI_BOLD"
-        >
-          5
-        </Text>
-        <Text
-          className="dots"
-          align="center"
-          color={Theme.colors.secondaryText}
-          fontName="REGULAR_SEMI_BOLD"
-        >
-          ...
-        </Text>
-        <Text
-          align="center"
-          color={Theme.colors.secondaryText}
-          fontName="REGULAR_SEMI_BOLD"
-        >
-          55
-        </Text>
-      </div>
+      <Pagination />
     </SalesContainer>
   );
 };
--- projects/reseller/src/app/sales/util/style.ts
+++ projects/reseller/src/app/sales/util/style.ts
@@ -7,37 +7,6 @@ export const SalesContainer = styled.div`
   flex-direction: column;
   align-items: center;
 
-  header {
-    height: 68px;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 0 28px;
-    background-color: ${Theme.colors.mainTransparent};
-
-    .headerTitle {
-      width: 161px;
-    }
-
-    .searchInput {
-      width: 100%;
-      max-width: 440px;
-    }
-
-    .filter {
-      width: 161px;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      padding: 0 16px;
-
-      span {
-        cursor: pointer;
-      }
-    }
-  }
-
   .titles {
     display: flex;
     align-items: center;
@@ -66,24 +35,5 @@ export const SalesContainer = styled.div`
 
   .cards {
     width: 100%;
-  }
-
-  .pagination {
-    display: flex;
-    width: 100%;
-    max-width: 270px;
-    flex-direction: row;
-    justify-content: center;
-    margin-top: 28px;
-    margin-bottom: 56px;
-
-    p {
-      margin: 0 16px;
-      cursor: pointer;
-    }
-
-    .dots {
-      cursor: default;
-    }
   }
 `;
