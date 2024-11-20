32be70b99037316349126b4d6238d89226c93177
thulerjoao <thuler_lima@hotmail.com>
2024-11-20 20:49:11 +0000

    feat(reseller): created mobile mobile menu

--- projects/reseller/src/public/components/mobileHeader/index.tsx
+++ projects/reseller/src/public/components/mobileHeader/index.tsx
@@ -1,20 +1,31 @@
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { scrollToTop } from "utils/scrollToTopFunction";
+import MobileMenu from "../mobileMenu";
 import Logo from "./icons/Logo.svg";
 import Menu from "./icons/Menu.svg";
 import { MobileHeaderContainer } from "./style";
 
 const MobiletHeader = () => {
   const route = useRouter();
+  const [openMenu, setOpenMenu] = useState<boolean>(false);
 
   return (
-    <MobileHeaderContainer>
-      <span onClick={() => scrollToTop()}>
-        <Logo />
-      </span>
-      <span>
-        <Menu />
-      </span>
+    <MobileHeaderContainer openMenu={openMenu}>
+      <section className="topCompoennt">
+        <span onClick={() => scrollToTop()}>
+          <Logo />
+        </span>
+        <span
+          onClick={(event) => {
+            event.preventDefault();
+            setOpenMenu(!openMenu);
+          }}
+        >
+          <Menu />
+        </span>
+      </section>
+      {openMenu && <MobileMenu setOpenMenu={setOpenMenu} />}
     </MobileHeaderContainer>
   );
 };
--- projects/reseller/src/public/components/mobileHeader/style.ts
+++ projects/reseller/src/public/components/mobileHeader/style.ts
@@ -1,19 +1,34 @@
 import { Theme } from "@4miga/design-system/theme/theme";
 import styled from "styled-components";
 
-export const MobileHeaderContainer = styled.header`
+interface Props {
+  openMenu: boolean;
+}
+
+export const MobileHeaderContainer = styled.header<Props>`
   width: 100%;
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: space-between;
-  height: 48px;
-  padding: 0 16px;
-  box-sizing: border-box;
-  background-color: ${Theme.colors.mainTransparent};
   position: fixed;
   top: 0;
   left: 0;
   z-index: 10;
+
+  .topCompoennt {
+    background-color: ${({ openMenu }) =>
+      openMenu ? Theme.colors.maindark : Theme.colors.mainTransparent};
+    border-bottom: ${({ openMenu }) =>
+      openMenu && `1px solid ${Theme.colors.secondaryAction}`};
+    box-sizing: border-box;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 48px;
+    height: ${({ openMenu }) => openMenu && "49px"};
+    padding: 0 16px;
+  }
 
   span {
     display: flex;
