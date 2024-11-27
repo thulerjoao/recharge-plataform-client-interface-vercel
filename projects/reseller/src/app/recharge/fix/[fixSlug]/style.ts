ddd4f7dcf4439f45893212e6c336c0d5e7220bc2
thulerjoao <thuler_lima@hotmail.com>
2024-11-27 20:25:56 +0000

    feat(reseller): created recharge/fix page responsive for all kind of devices

--- projects/reseller/src/app/recharge/fix/[fixSlug]/style.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const FixRechargePage = styled.div`
-  @media (min-width: 768px) and (max-width: 1024px) {
-  }
-  @media (max-width: 767px) {
-  }
-`;
