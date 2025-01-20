c3e301c381bf99822ff01f828e70cc3284be6f37
thulerjoao <thuler_lima@hotmail.com>
2025-01-20 20:13:16 +0000

    feat(packages): implemented service api to recieve baseUrl

--- packages/services/connectionAPI/connection.ts
+++ packages/services/connectionAPI/connection.ts
@@ -1,31 +1,40 @@
 import ConnectionAPI from "./Axios/axiosConnection";
 import { MethodEnum } from "./Axios/methodEnum";
 
-export const connectionAPIGet = async <T>(url: string): Promise<T> => {
-  return ConnectionAPI.connect(url, MethodEnum.GET);
+export const connectionAPIGet = async <T>(
+  url: string,
+  baseURL: string,
+): Promise<T> => {
+  return ConnectionAPI.connect(url, MethodEnum.GET, undefined, baseURL);
 };
 
-export const connectionAPIDelete = async <T>(url: string): Promise<T> => {
-  return ConnectionAPI.connect(url, MethodEnum.DELETE);
+export const connectionAPIDelete = async <T>(
+  url: string,
+  baseURL: string,
+): Promise<T> => {
+  return ConnectionAPI.connect(url, MethodEnum.DELETE, undefined, baseURL);
 };
 
 export const connectionAPIPost = async <T>(
   url: string,
   body: unknown,
+  baseURL: string,
 ): Promise<T> => {
-  return ConnectionAPI.connect(url, MethodEnum.POST, body);
+  return ConnectionAPI.connect(url, MethodEnum.POST, body, baseURL);
 };
 
 export const connectionAPIPut = async <T>(
   url: string,
   body: unknown,
+  baseURL: string,
 ): Promise<T> => {
-  return ConnectionAPI.connect(url, MethodEnum.PUT, body);
+  return ConnectionAPI.connect(url, MethodEnum.PUT, body, baseURL);
 };
 
 export const connectionAPIPatch = async <T>(
   url: string,
   body: unknown,
+  baseURL: string,
 ): Promise<T> => {
-  return ConnectionAPI.connect(url, MethodEnum.PATCH, body);
+  return ConnectionAPI.connect(url, MethodEnum.PATCH, body, baseURL);
 };
