28df0e4d465f1368f849eaa2662765c2a8d2145b
thulerjoao <thuler_lima@hotmail.com>
2025-02-14 20:50:29 +0000

    pakcage: serviceaxios instance updated to work with credentials(without passing token)

--- packages/services/connectionAPI/Axios/axiosInstace.ts
+++ packages/services/connectionAPI/Axios/axiosInstace.ts
@@ -3,24 +3,12 @@ import axios, { AxiosInstance } from "axios";
 const createApiInstance = (baseURL: string): AxiosInstance => {
   const api = axios.create({
     baseURL,
+    withCredentials: true, // 🔥 Permite que cookies sejam enviados automaticamente
   });
 
-  api.interceptors.request.use(async (config: any) => {
-    try {
-      const getToken = async () => {
-        return sessionStorage.getItem("token");
-      };
-      const token = await getToken();
-
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-        config.headers.contentType = "application/json";
-      }
-
-      return config;
-    } catch (error) {
-      /* empty */
-    }
+  api.interceptors.request.use((config) => {
+    config.headers["Content-Type"] = "application/json";
+    return config;
   });
 
   return api;
@@ -28,22 +16,32 @@ const createApiInstance = (baseURL: string): AxiosInstance => {
 
 export default createApiInstance;
 
-// type Body = {
-//   accessToken?: string
-//   refreshToken?: string
-// }
+// import axios, { AxiosInstance } from "axios";
 
-// type HttpResponse = {
-//   statusCode: number
-//   body: Body
-// }
+// const createApiInstance = (baseURL: string): AxiosInstance => {
+//   const api = axios.create({
+//     baseURL,
+//   });
 
-// const httpDecorator = (response: HttpResponse): HttpResponse => {
-//   if (response.body.accessToken) {
-//     setLocalStorage('accessToken', response.body.accessToken)
-//   }
-//   if (response.body.refreshToken) {
-//     setCookie('refreshToken', response.body.refreshToken)
-//   }
-//   return response
-// }
+//   api.interceptors.request.use(async (config: any) => {
+//     try {
+//       const getToken = async () => {
+//         return sessionStorage.getItem("token");
+//       };
+//       const token = await getToken();
+
+//       if (token) {
+//         config.headers.Authorization = `Bearer ${token}`;
+//         config.headers.contentType = "application/json";
+//       }
+
+//       return config;
+//     } catch (error) {
+//       /* empty */
+//     }
+//   });
+
+//   return api;
+// };
+
+// export default createApiInstance;
