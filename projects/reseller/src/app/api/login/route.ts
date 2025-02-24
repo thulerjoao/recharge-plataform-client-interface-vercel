8fecf0661848eb3786db25d03dc9ada3b5702ba0
thulerjoao <thuler_lima@hotmail.com>
2025-02-24 21:10:35 +0000

    feat(reseller): implemented middleware to block routes when not logged

--- projects/reseller/src/app/api/login/route.ts
+++ projects/reseller/src/app/api/login/route.ts
@@ -22,6 +22,13 @@ export async function POST(req: Request) {
       ...(rememberMe && { maxAge: 60 * 60 * 24 * 365 * 10 }),
     });
 
+    cookieStore.set("logged", "true", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      path: "/",
+    });
+
     return NextResponse.json({ success: true });
   } catch (error) {
     return NextResponse.json({ error: "Invalid request" }, { status: 400 });
