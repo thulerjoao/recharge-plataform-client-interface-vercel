c14da56f846a3d54d2520cdda2b9a0e8142c1e70
thulerjoao <thuler_lima@hotmail.com>
2025-03-06 21:58:22 +0000

    feat(store): implemented context to catch products from api

---
+++ projects/store/src/contexts/product/index.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { connectionAPIGet } from "@4miga/services/connectionAPI/connection";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { ProductType } from "types/globalTypes";
+import { apiUrl } from "utils/apiUrl";
+
+interface ProductContextProps {
+  products: ProductType[];
+  updateProducts: () => void;
+}
+
+const ProductContext = createContext<ProductContextProps | undefined>(
+  undefined,
+);
+
+export const ProductProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<ProductType[]>(null);
+
+  const updateProducts = () => {
+    connectionAPIGet<ProductType[]>("/product", apiUrl).then((res) => {
+      if (!res) throw new Error("Erro ao busca produtos");
+      setProducts(res);
+    });
+  };
+
+  useEffect(() => {
+    updateProducts;
+  }, []);
+
+  console.log(products);
+
+  return (
+    <ProductContext.Provider
+      value={{
+        products,
+        updateProducts,
+      }}
+    >
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+export const useProduct = (): ProductContextProps => {
+  const context = useContext(ProductContext);
+  if (!context) {
+    throw new Error("useProduct must be used within a ProductProvider");
+  }
+  return context;
+};
--- projects/store/src/types/globalTypes.ts
+++ projects/store/src/types/globalTypes.ts
@@ -1,3 +1,4 @@
+//=========== types =============
 export type DeviceType = "mobile" | "tablet" | "desktop";
 
 export type UserType = {
@@ -5,6 +6,31 @@ export type UserType = {
   email: string;
   cpf?: string;
 };
+
+export type ProductType = {
+  name: string;
+  about: string;
+  instructions: string;
+  imgBannerUrl: string;
+  imgCardUrl: string;
+  packages: PackagesType[];
+};
+
+export type PackagesType = {
+  name: string;
+  amountCredits: number;
+  imgCardUrl: string;
+  isOffer: boolean;
+  baseCost: number;
+  paymentMethods: PaymentMethodsType[];
+};
+
+export type PaymentMethodsType = {
+  name: string;
+  rate: number;
+};
+
+//=========== interfaces =============
 
 export interface loginParams {
   email: string;
