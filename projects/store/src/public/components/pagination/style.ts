b7eda9fc95015199b98be8581715c8a025fe4343
thulerjoao <thuler_lima@hotmail.com>
2025-04-08 22:35:56 +0000

    feat(store): updated width in pagination component to fit better with 1 and 2 pages

--- projects/store/src/public/components/pagination/style.ts
+++ projects/store/src/public/components/pagination/style.ts
@@ -1,12 +1,19 @@
 import styled from "styled-components";
 
-export const PaginationContainer = styled.div`
+interface Props {
+  totalPages: number;
+}
+
+export const PaginationContainer = styled.div<Props>`
   display: flex;
   width: 100%;
   max-width: 270px;
   flex-direction: row;
   justify-content: center;
   margin-top: 32px;
+
+  ${(item) => item.totalPages === 2 && "width: 100px"};
+  ${(item) => item.totalPages === 3 && "width: 170px"};
 
   p {
     margin: 0 16px;
