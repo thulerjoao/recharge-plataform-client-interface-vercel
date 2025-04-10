ad20afb7b22e9136e75fdb85be8646e0f3087c00
thulerjoao <thuler_lima@hotmail.com>
2025-04-10 20:43:22 +0000

    feat(store): created page and route to update external api url connection to be used in tests

--- packages/services/connectionAPI/Axios/axiosInstace.ts
+++ packages/services/connectionAPI/Axios/axiosInstace.ts
@@ -4,8 +4,22 @@ import axios, {
   InternalAxiosRequestConfig,
 } from "axios";
 
-const createApiInstance = (baseURL: string): AxiosInstance => {
-  const api = axios.create({ baseURL });
+const createApiInstance = async (baseURL: string): Promise<AxiosInstance> => {
+  const getUrl = async () => {
+    try {
+      const response = await axios.get("/api/apiurl", {
+        withCredentials: true,
+      });
+      return response.data?.apiUrl ?? null;
+    } catch {
+      return null;
+    }
+  };
+
+  const cookiesApiUrl = await getUrl();
+  const chosenUrl = cookiesApiUrl ? `https://${cookiesApiUrl}` : baseURL;
+  console.log(chosenUrl);
+  const api = axios.create({ baseURL: chosenUrl });
 
   api.interceptors.request.use(
     async (config: InternalAxiosRequestConfig) => {
@@ -37,3 +51,43 @@ const createApiInstance = (baseURL: string): AxiosInstance => {
 };
 
 export default createApiInstance;
+
+// import axios, {
+//   AxiosError,
+//   AxiosInstance,
+//   InternalAxiosRequestConfig,
+// } from "axios";
+
+// const createApiInstance = (baseURL: string): AxiosInstance => {
+//   const api = axios.create({ baseURL });
+
+//   api.interceptors.request.use(
+//     async (config: InternalAxiosRequestConfig) => {
+//       try {
+//         const response = await axios.get("/api/token", {
+//           withCredentials: true,
+//         });
+//         const accessToken = response.data?.accessToken;
+//         if (accessToken) {
+//           config.headers.set("Authorization", `Bearer ${accessToken}`);
+//         }
+//         config.headers.set("Content-Type", "application/json");
+//         config.headers.set("ngrok-skip-browser-warning", "true");
+
+//         return config;
+//       } catch (error) {
+//         return config;
+//       }
+//     },
+//     (error) => Promise.reject(error),
+//   );
+
+//   api.interceptors.response.use(
+//     (response) => response,
+//     (error: AxiosError) => Promise.reject(error),
+//   );
+
+//   return api;
+// };
+
+// export default createApiInstance;
