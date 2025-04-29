e7022b78c2c1a6dc22009a93cd0fbc9f218974d1
thulerjoao <thuler_lima@hotmail.com>
2025-04-29 17:50:01 +0000

    fix(bigo) wrong slug in packages page fixed

--- projects/bigo/src/app/package/[slug]/page.tsx
+++ projects/bigo/src/app/package/[slug]/page.tsx
@@ -3,13 +3,11 @@ import PaymentPage from "./index";
 type Props = {
   params: {
     slug: string;
-    secondarySlug: string;
   };
 };
 
 const Page = async ({ params }: Props) => {
-  const id = params.secondarySlug;
   const slug = params.slug;
-  return <PaymentPage id={id} slug={slug} />;
+  return <PaymentPage slug={slug} />;
 };
 export default Page;
