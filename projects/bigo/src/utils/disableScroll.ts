f905ed6bc0093ff0be58be2201a69996a44a25fe
thulerjoao <thuler_lima@hotmail.com>
2025-05-27 19:04:23 +0000

    fix(store): updated disableScroll function

--- projects/bigo/src/utils/disableScroll.ts
+++ projects/bigo/src/utils/disableScroll.ts
@@ -1,13 +1,29 @@
+let scrollY = 0;
+
 export function disableScroll(lock: boolean) {
   if (typeof window === "undefined") return;
 
+  const body = document.body;
+
   if (lock) {
+    scrollY = window.scrollY;
     const scrollBarWidth =
       window.innerWidth - document.documentElement.clientWidth;
-    document.body.style.overflowY = "hidden";
-    document.body.style.paddingRight = `${scrollBarWidth}px`;
+
+    body.style.position = "fixed";
+    body.style.top = `-${scrollY}px`;
+    body.style.left = "0";
+    body.style.right = "0";
+    body.style.overflowY = "hidden";
+    body.style.paddingRight = `${scrollBarWidth}px`;
   } else {
-    document.body.style.overflowY = "";
-    document.body.style.paddingRight = "";
+    body.style.position = "";
+    body.style.top = "";
+    body.style.left = "";
+    body.style.right = "";
+    body.style.overflowY = "";
+    body.style.paddingRight = "";
+
+    window.scrollTo(0, scrollY);
   }
 }
