d50e56871958cf4e6863405524d621520964e5d5
thulerjoao <thuler_lima@hotmail.com>
2025-07-19 23:03:33 +0000

    linked bigo and store with api in deploy

--- projects/store/src/utils/apiUrl.ts
+++ projects/store/src/utils/apiUrl.ts
@@ -1,3 +1,5 @@
-export const apiUrl: string = "http://localhost:3333";
+// export const apiUrl: string = "http://localhost:3333";
+export const apiUrl: string =
+  "https://recharge-plataform-api-production.up.railway.app";
 
 export const storeId: string = "2c0824d8-e8cd-418c-9f77-558343a1283c";
