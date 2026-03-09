c92c3c1235a001acac70becef14dca6d9d518121
thulerjoao <thuler_lima@hotmail.com>
2026-03-09 18:50:48 -0300

    fix(reseller): fixed prettier problems

--- projects/reseller/src/app/products/page.tsx
+++ projects/reseller/src/app/products/page.tsx
@@ -1,18 +1,18 @@
 import ProductsPage from "./util";
 import { Suspense } from "react";
- 
- const Page = () => {
-   return (
-     <Suspense
-       fallback={
-         <div className="container">
-           <span className="loading" />
-         </div>
-       }
-     >
-       <ProductsPage />
-     </Suspense>
-   );
- };
- 
- export default Page;
+
+const Page = () => {
+  return (
+    <Suspense
+      fallback={
+        <div className="container">
+          <span className="loading" />
+        </div>
+      }
+    >
+      <ProductsPage />
+    </Suspense>
+  );
+};
+
+export default Page;
